d496111b620f11fafa955787ea32366f619d65af
GailGoncalves <gonc6532@outlook.com>
2019-03-29 17:26:05 -0700

    header and footer

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,9 @@
-// Insert JavaScript here...
+// Responsive Menu - Dropdown
+const body = document.body;
+const hamburger = document.getElementById('hamburger');
+
+hamburger.addEventListener('click', openMenu);
+
+function openMenu(){
+    body.classList.toggle('show');
+}
