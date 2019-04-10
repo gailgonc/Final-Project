8123775b3694448b0bf22bd33af3f982796eb697
GailGoncalves <gonc6532@outlook.com>
2019-04-10 16:35:32 -0700

    Fixed index page title when hamburger used

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -5,11 +5,16 @@ window.onload=function(){
     
     const sticker = document.querySelector('.rotating-sticker');
     const hamburger = document.getElementById('hamburger');
+    const titleText = document.getElementById('picture-text');
         
     hamburger.addEventListener('click', openMenu);
 
     function openMenu(){
         body.classList.toggle('show');
+        console.log(titleText.classList);
+        titleText.classList.toggle('banner-child-location');
+        console.log(titleText.classList);
+       
     }
     
 
